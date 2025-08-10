d7b3ce7859f88d30c7a1efc25bc73b69f8e96734
Dullat <142017979+Dullat@users.noreply.github.com>
2025-08-10 17:15:52 +0530

    sync vs async

---
+++ code/1.syncVsAsync.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+console.log('Sync Read Starts...');
+const dataSync = fs.readFileSync('sample.txt', 'utf8')
+console.log(dataSync)
+console.log('Sync read is Done...')
+
+console.log('Async read begins...');
+const dataAsync = fs.readFile('sample.txt', 'utf8', (err, data) => {
+    if(err){
+        console.log(err)
+    }
+    if(data){
+        console.log(data)
+    }
+})
+console.log('programe instructions end...')
