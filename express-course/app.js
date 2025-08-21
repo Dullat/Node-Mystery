4cff7629849de057d19d7b7913eb0c7fd426df51
dullat <142017979+Dullat@users.noreply.github.com>
2025-08-21 20:45:54 +0530

    serving nav bar with raw node, no express

--- express-course/app.js
+++ express-course/app.js
@@ -2,9 +2,11 @@ const http = require("http");
 const fs = require("fs");
 
 // get all files
-const homePage = fs.readFileSync("./index.html");
+const homePage = fs.readFileSync("./navbar-app/index.html");
+const styles = fs.readFileSync("./navbar-app/styles.css");
 
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   if (req.url === "/") {
     res.writeHead(200, {
       "Content-Type": "text/html",
@@ -16,6 +18,10 @@ const server = http.createServer((req, res) => {
       "Content-Type": "text/html",
     });
     res.write("<h1>about page</h1>");
+    res.end();
+  } else if (req.url === "/styles.css") {
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.write(styles);
     res.end();
   } else {
     res.writeHead(404);
