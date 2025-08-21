4e81b045dbb6f8704e39cc21c4237755c593b043
dullat <142017979+Dullat@users.noreply.github.com>
2025-08-21 20:22:25 +0530

    serving simple html

--- express-course/app.js
+++ express-course/app.js
@@ -1,14 +1,28 @@
-const http = require('http')
+const http = require("http");
+const fs = require("fs");
+
+// get all files
+const homePage = fs.readFileSync("./index.html");
 
 const server = http.createServer((req, res) => {
-  console.log('user hit the server')
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-  res.write('<h1>Home page</h1>')
-  res.end()
-})
+  if (req.url === "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.write(homePage);
+    res.end();
+  } else if (req.url === "/about") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.write("<h1>about page</h1>");
+    res.end();
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
 
-server.listen(5000, () =>{
-  console.log('runing on 5000')
-})
+server.listen(5000, () => {
+  console.log("runing on 5000");
+});
