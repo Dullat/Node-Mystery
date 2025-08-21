2e4eccd868f2404a9bb3c05a60408c0dc42eb0c2
dullat <142017979+Dullat@users.noreply.github.com>
2025-08-21 19:15:45 +0530

    theory

--- express-course/app.js
+++ express-course/app.js
@@ -2,7 +2,11 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
   console.log('user hit the server')
-  res.end('home page')
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  })
+  res.write('<h1>Home page</h1>')
+  res.end()
 })
 
 server.listen(5000, () =>{
