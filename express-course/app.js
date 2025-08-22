3f258c316b8dbdf94ad07e2dd771fb820868ba79
dullat <142017979+Dullat@users.noreply.github.com>
2025-08-22 16:16:21 +0530

    query params

--- express-course/app.js
+++ express-course/app.js
@@ -6,7 +6,34 @@ const { products } = require("./data.js");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get(`/api/products/:productID`, (req, res) => {
+  const { productID } = req.params;
+  const id = Number(productID);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid product ID" });
+  }
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID),
+  );
+
+  if (!singleProduct) {
+    return res.status(404).json({ error: "Product does not exist" });
+  }
+
+  res.json(singleProduct);
 });
 
 app.all("/*splat", (req, res) => {
