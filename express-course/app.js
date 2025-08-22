8289976e56af65c0a85fe94f285d892ffb9757a1
dullat <142017979+Dullat@users.noreply.github.com>
2025-08-22 10:11:05 +0530

    serving files with express

--- express-course/app.js
+++ express-course/app.js
@@ -1,18 +1,17 @@
 const express = require("express");
+const path = require("path");
 const { readFileSync } = require("fs");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+app.use(express.static("./public"));
 
-app.get("/about", (req, res) => {
-  res.send("blob");
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("/*splat", (req, res) => {
-  res.status(404).send("Page not found");
+  res.status(404).send("page not found");
 });
 
 app.listen(5000, () => {
