2342e1925d607d8058700f1d30d8d2abf9af99fc
dullat <142017979+Dullat@users.noreply.github.com>
2025-08-22 16:20:02 +0530

    query params save

--- express-course/final/06-basic-json.js
+++ express-course/final/06-basic-json.js
@@ -1,10 +1,45 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
-app.get('/', (req, res) => {
-  res.json(products)
-})
+const express = require("express");
+const path = require("path");
+const { readFileSync } = require("fs");
+const { products } = require("./data.js");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get(`/api/products/:productID`, (req, res) => {
+  const { productID } = req.params;
+  const id = Number(productID);
+
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "Invalid product ID" });
+  }
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID),
+  );
+
+  if (!singleProduct) {
+    return res.status(404).json({ error: "Product does not exist" });
+  }
+
+  res.json(singleProduct);
+});
+
+app.all("/*splat", (req, res) => {
+  res.status(404).send("page not found");
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("server is listening on 5000");
+});
