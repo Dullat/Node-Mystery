bc95879f6c24dc76dc3f8610ff062a79f339469d
dullat <142017979+Dullat@users.noreply.github.com>
2025-08-29 23:50:32 +0530

    querying

--- 02-store-api/starter/controllers/products.js
+++ 02-store-api/starter/controllers/products.js
@@ -1,12 +1,27 @@
+const Product = require('../models/product')
 const { createCustomError } = require("../errors/customError")
 
 const getAllProductsStatic = async (req, res, next) => {
-    throw createCustomError("Dummy error", 505)
-    res.status(200).json({msg: 'products testing routs'})
-} 
-const getAllProducts = async (req, res, next) => {
-    res.status(200).json({msg: 'products route'})
+    const search = req.query.name
+    const products = await Product.find({})
+        .sort('-name price')
+    res.status(200).json(products)
 }
+const getAllProducts = async (req, res, next) => {
+    const { featured, company, name } = req.query
+    console.log(name)
+    const queryObject = {
+        ...(featured === 'true' || featured === 'false' ? { featured } : {}),
+        ...(company && { company }),
+        ...(name && { name: { $regex: name, $options: 'i' } })
+    }
+
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
+    res.status(200).json({ products, nbHits: products.length })
+}
+
+
 
 module.exports = {
     getAllProducts,
