1719ded4925dff85a26cf8f214646c9a489c69df
dullat <142017979+Dullat@users.noreply.github.com>
2025-08-30 19:58:51 +0530

    sorting and limit

--- 02-store-api/starter/controllers/products.js
+++ 02-store-api/starter/controllers/products.js
@@ -1,29 +1,48 @@
-const Product = require('../models/product')
-const { createCustomError } = require("../errors/customError")
+const Product = require("../models/product");
+const { createCustomError } = require("../errors/customError");
 
 const getAllProductsStatic = async (req, res, next) => {
-    const search = req.query.name
-    const products = await Product.find({})
-        .sort('-name price')
-    res.status(200).json(products)
-}
+  const search = req.query.name;
+  const products = await Product.find({})
+    .sort("-name price")
+    .select("name")
+    .limit(10);
+  res.status(200).json(products);
+};
 const getAllProducts = async (req, res, next) => {
-    const { featured, company, name } = req.query
-    console.log(name)
-    const queryObject = {
-        ...(featured === 'true' || featured === 'false' ? { featured } : {}),
-        ...(company && { company }),
-        ...(name && { name: { $regex: name, $options: 'i' } })
-    }
+  const { featured, company, name, sort, fields } = req.query;
 
-    console.log(queryObject)
-    const products = await Product.find(queryObject)
-    res.status(200).json({ products, nbHits: products.length })
-}
+  const queryObject = {
+    ...(featured === "true" || featured === "false" ? { featured } : {}),
+    ...(company && { company }),
+    ...(name && { name: { $regex: name, $options: "i" } }),
+  };
 
+  console.log(req.query);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
 
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 module.exports = {
-    getAllProducts,
-    getAllProductsStatic
-}
+  getAllProducts,
+  getAllProductsStatic,
+};
+
--- 02-store-api/starter/middleware/error-handler.js
+++ 02-store-api/starter/middleware/error-handler.js
@@ -1,10 +1,13 @@
-const { CustomAPIError } = require("../errors/customError")
+const { CustomAPIError } = require("../errors/customError");
 
 const errorHandlerMiddleware = async (err, req, res, next) => {
-  if(err instanceof CustomAPIError){
-    return res.status(err.statusCode).json({err: err, msg : err.message})
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ err: err, msg: err.message });
   }
-  return res.status(500).json({ msg: 'Something went wrong, please try again', err: err })
-}
+  return res.status(500).json({
+    msg: "Something went wrong, please try again",
+    err: err.message,
+  });
+};
 
-module.exports = errorHandlerMiddleware
+module.exports = errorHandlerMiddleware;
