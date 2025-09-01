a993a59e75d04470b9df9f4205cfb978624aa424
dullat <142017979+Dullat@users.noreply.github.com>
2025-09-01 22:36:16 +0530

    jobs-api: basic setup

---
+++ 04-jobs-api/starter/controllers/jobs.js
@@ -0,0 +1,23 @@
+const getAllJobs = async (req, res) => {
+  res.send("get all the jobs");
+};
+const getJob = async (req, res) => {
+  res.send("get all the jobs");
+};
+const createJob = async (req, res) => {
+  res.send("get all the jobs");
+};
+const updateJob = async (req, res) => {
+  res.send("get all the jobs");
+};
+const deleteJob = async (req, res) => {
+  res.send("get all the jobs");
+};
+
+module.exports = {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
