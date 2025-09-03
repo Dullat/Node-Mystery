c610fb0e1a41c08662a7e9c501c69e18ba70392b
dullat <142017979+Dullat@users.noreply.github.com>
2025-09-03 18:09:35 +0530

    security: added

--- 04-jobs-api/starter/app.js
+++ 04-jobs-api/starter/app.js
@@ -11,7 +11,23 @@ const authMiddleware = require("./middleware/authentication.js");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// extra security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
