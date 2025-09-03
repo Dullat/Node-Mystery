e05361c6031438e94eb8bd00e6e54f614018ded1
dullat <142017979+Dullat@users.noreply.github.com>
2025-09-03 16:49:05 +0530

    mongoose errors: cast, duplicate and validation

--- 04-jobs-api/starter/middleware/error-handler.js
+++ 04-jobs-api/starter/middleware/error-handler.js
@@ -1,12 +1,40 @@
-const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || "Something went wrong",
+  };
+
+  // Mongoose Validation Error
+  if (err.name === "ValidationError") {
+    customError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
   }
-  return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ err: err.message });
+
+  // Mongoose Duplicate Key Error
+  if (err.code && err.code === 11000) {
+    customError.message = `Duplicate value entered for '${Object.keys(
+      err.keyValue,
+    )}' field. Please choose another value.`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  // Mongoose Cast Error
+  if (err.name === "CastError") {
+    customError.message = `No item found with id : ${err.value}`;
+    customError.statusCode = 400;
+  }
+
+  // Send response
+  return res.status(customError.statusCode).json({
+    success: false,
+    statusCode: customError.statusCode,
+    message: customError.message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+  });
 };
 
 module.exports = errorHandlerMiddleware;
--- 04-jobs-api/starter/models/User.js
+++ 04-jobs-api/starter/models/User.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, "Please provide name"],
+    required: [true, "Please provide password"],
   },
 });
 
